e9bc1e7e2ae7eacbaf71d24d5b74434469aef343
AtherCoder99 <a.ngaming102734@gmail.com>
2026-05-08 18:12:33 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h2 className='text-amber-900 rounded'>React Class</h2>
+    </>
+  )
+}
+
+export default App
